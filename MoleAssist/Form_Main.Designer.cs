dcc1e9cd09c7dbc12eb22abd80651a4d9ddd8617
aprikyblue <aprikyblue@gmail.com>
2015-08-20 21:48:57 +0800

    发布准备 20150820

--- MoleAssist/Form_Main.Designer.cs
+++ MoleAssist/Form_Main.Designer.cs
@@ -65,20 +65,6 @@
             this.btn_clearCache = new System.Windows.Forms.Button();
             this.btn_refreshGame = new System.Windows.Forms.Button();
             this.checkBox_onTop = new System.Windows.Forms.CheckBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
-            this.button11 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button12 = new System.Windows.Forms.Button();
-            this.button13 = new System.Windows.Forms.Button();
             label_skillOrder = new System.Windows.Forms.Label();
             label_disclaimer = new System.Windows.Forms.Label();
             label_about = new System.Windows.Forms.Label();
@@ -470,145 +456,6 @@
             this.checkBox_onTop.UseVisualStyleBackColor = true;
             this.checkBox_onTop.CheckedChanged += new System.EventHandler(this.checkBox_onTop_CheckedChanged);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(572, 687);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(61, 27);
-            this.button1.TabIndex = 14;
-            this.button1.Text = "获得物品";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(639, 687);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(61, 27);
-            this.button2.TabIndex = 15;
-            this.button2.Text = "关闭资料";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(706, 687);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(63, 27);
-            this.button3.TabIndex = 16;
-            this.button3.Text = "战斗结束";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(775, 687);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(97, 27);
-            this.button4.TabIndex = 17;
-            this.button4.Text = "技能或等级提升";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(878, 687);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(62, 27);
-            this.button5.TabIndex = 18;
-            this.button5.Text = "在线时间";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(946, 687);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(66, 27);
-            this.button6.TabIndex = 19;
-            this.button6.Text = "首发死亡";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(1018, 687);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(61, 27);
-            this.button7.TabIndex = 20;
-            this.button7.Text = "寻找野怪";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(1085, 687);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(61, 27);
-            this.button8.TabIndex = 21;
-            this.button8.Text = "战斗加载";
-            this.button8.UseVisualStyleBackColor = true;
-            this.button8.Click += new System.EventHandler(this.button8_Click);
-            // 
-            // button9
-            // 
-            this.button9.Location = new System.Drawing.Point(572, 729);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(109, 27);
-            this.button9.TabIndex = 22;
-            this.button9.Text = "是否可以使用技能";
-            this.button9.UseVisualStyleBackColor = true;
-            this.button9.Click += new System.EventHandler(this.button9_Click);
-            // 
-            // button10
-            // 
-            this.button10.Location = new System.Drawing.Point(687, 729);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(61, 27);
-            this.button10.TabIndex = 23;
-            this.button10.Text = "验证框";
-            this.button10.UseVisualStyleBackColor = true;
-            this.button10.Click += new System.EventHandler(this.button10_Click);
-            // 
-            // button11
-            // 
-            this.button11.Location = new System.Drawing.Point(754, 729);
-            this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(61, 27);
-            this.button11.TabIndex = 24;
-            this.button11.Text = "替换精灵";
-            this.button11.UseVisualStyleBackColor = true;
-            this.button11.Click += new System.EventHandler(this.button11_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(843, 738);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 12);
-            this.label1.TabIndex = 25;
-            this.label1.Text = "label1";
-            // 
-            // button12
-            // 
-            this.button12.Location = new System.Drawing.Point(946, 729);
-            this.button12.Name = "button12";
-            this.button12.Size = new System.Drawing.Size(104, 40);
-            this.button12.TabIndex = 26;
-            this.button12.Text = "重新加载Lua";
-            this.button12.UseVisualStyleBackColor = true;
-            this.button12.Click += new System.EventHandler(this.button12_Click);
-            // 
-            // button13
-            // 
-            this.button13.Location = new System.Drawing.Point(865, 759);
-            this.button13.Name = "button13";
-            this.button13.Size = new System.Drawing.Size(75, 23);
-            this.button13.TabIndex = 27;
-            this.button13.Text = "button13";
-            this.button13.UseVisualStyleBackColor = true;
-            this.button13.Click += new System.EventHandler(this.button13_Click);
-            // 
             // Form_Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -616,20 +463,6 @@
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(1201, 785);
-            this.Controls.Add(this.button13);
-            this.Controls.Add(this.button12);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button11);
-            this.Controls.Add(this.button10);
-            this.Controls.Add(this.button9);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox_control);
             this.Controls.Add(this.tabControl_Main);
             this.Controls.Add(this.webBrowser_game);
@@ -651,7 +484,6 @@
             this.groupBox_control.ResumeLayout(false);
             this.groupBox_control.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -684,20 +516,6 @@
         private System.Windows.Forms.ComboBox combo_identifyFailed;
         private System.Windows.Forms.CheckBox checkBox_isAutoIdentify;
         private System.Windows.Forms.CheckBox checkBox_ReHP;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.Button button11;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button12;
-        private System.Windows.Forms.Button button13;
     }
 }
 
