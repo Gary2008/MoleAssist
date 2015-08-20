764828b33f659319ac6966981366cfb821cceedf
aprikyblue <aprikyblue@gmail.com>
2015-08-20 21:03:39 +0800

    更新lua脚本

--- Config/fight.lua
+++ Config/fight.lua
@@ -2,63 +2,93 @@ import ('System')
 import ('System.Windows.Forms')
 import ('System.Drawing')
 
--- these following variable will be assigned when StartFight
-local handle
-local FightType
--- local interval -- number
-local skillMode -- number
-local skillOrder -- string
--- local identifyFailed -- number
--- local ReHP -- boolean
--- local useAnger -- boolean
--- local alert -- boolean
--- local qucikTraining -- boolean
--- local couldHiddenMode -- boolean
-
-
 function MsgBox(str)  
 	MessageBox.Show(str, "")
 end
 
+_G.use = 0 -- used skill index
+_G.skillTable = {}
+
+-- function initialize these values
 function Set()
-	handle = FightOptions.handle
-	FightType = FightOptions.type
-	-- interval = FightOptions.interval
-	skillMode = FightOptions.skillMode
-	skillOrder = FightOptions.skillOrder
-	-- identifyFailed = FightOptions.identifyFailed
-	ReHP = FightOptions.ReHP
-	-- useAnger = FightOptions.useAnger
-	-- alert = FightOptions.alert
-	-- qucikTraining = FightOptions.qucikTraining
-	-- couldHiddenMode = FightOptions.couldHiddenMode
-	local s = tonumber(FightOptions.skillOrder)
+	Trace("Set()")
+	-- these following variable will be assigned when press StartFight button
+	-- FightOptions.handle
+	-- FightOptions.type -- string
+	-- FightOptions.interval
+	-- FightOptions.skillMode -- number
+	-- FightOptions.skillOrder -- string
+	-- FightOptions.identifyFailed -- number
+	-- FightOptions.ReHP -- boolean
+	-- FightOptions.useAnger -- boolean
+	-- FightOptions.alert -- boolean
+	-- FightOptions.qucikTraining -- boolean
+	-- FightOptions.couldHiddenMode -- boolean
+	local skillOrder = FightOptions.skillOrder
+	if (skillOrder == "") then
+		skillOrder = "1"
+	end
+	Trace("skillOrderLen"..tostring(string.len(skillOrder)))
+	-- lua index starts from 1
+	for i=1, string.len(skillOrder) do
+		-- get the char from position i to i
+		Trace("ST"..tostring(i).. tonumber( string.sub(skillOrder, i, i)) )
+		_G.skillTable[i] = tonumber( string.sub(skillOrder, i, i) )
+	end
 end
 
 function Fight()
+	Trace("Fight()")
+	Trace(FightOptions.type)
+	use = use + 1
+	if (FightOptions.skillMode == 0 or use > #skillTable) then
+	 	use = 1
+	end
+	-- Trace("2")
 	if FightCall_couldUseSkill() then
-		FightCall_UseSkill(0)
+		Trace("UseI"..tostring(use))
+		Trace("Use"..tostring(skillTable[use]))
+		FightCall_UseSkill(skillTable[use])
 	end
+	-- Trace("3")
 	FightCall_OnlineTime()
+	-- Trace("3")
 	FightCall_GetGoods()
+	-- Trace("4")
 	FightCall_Profile()
+	-- Trace("5")
 	FightCall_SkillLvUp()
+	-- Trace("6")
 	FightCall_hasVerify()
-	FightCall_FightEnd()
-	if ReHP or FightCall_PetDie() then
-		FightCall_FullHP()
+	-- Trace("7")
+	if FightCall_FightEnd() == true then
+		use = 0
+		Trace("Clear")
 	end
-	if FightType == "Wild" then
+	--Trace("8")
+	if FightOptions.type == "Wild" then
+		-- Trace("9")
 		FightWild()
-	elseif FightType == "NPC" then
+		-- Trace("10")
+	elseif FightOptions.type == "NPC" then
+		-- Trace("11")
 		FightNPC(FightOptions.NPC)
-	elseif FightType == "CustomPoint" then
+		-- Trace("12")
+	elseif FightOptions.type == "CustomPoint" then
+		-- Trace("13")
 		FightCustomPoint(FightOptions.customPoint)
+		-- Trace("14")
+	end
+	if FightOptions.ReHP and FightCall_PetDie() then
+		-- Trace("15")
+		FightCall_FullHP()
+		-- Trace("16")
 	end
 end
 
 
 function FightWild()
+	Trace("Find!")
 	FightCall_FindAndClickMonster()
 end
 
